cb28e722a19f4888c6848141fb360690fdff880a
moonbot <moonbot@moonbot.home>
2015-06-25 12:25:56 -0400

    Cleaned up a lot

--- getdaily.js
+++ getdaily.js
@@ -1,56 +1,31 @@
 #!/usr/bin/env node
 
-/*
-
-TODO:
-Completely refactor.
-Should only make one call to youtube get info, then keep refering to that.
-
-*/
-
 var youtubedl = require('youtube-dl');
 var async = require('async')
 var fs = require('fs')
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+var format = "vhttp-200"
+//var format = "vhttp-3500"
 
 var url
-//var url = "http://thedailyshow.cc.com/full-episodes/8mfir8/june-16--2015---aziz-ansari"
-//var url = "http://thedailyshow.cc.com/full-episodes/drk62j/june-15--2015---judd-apatow"
-//var url = "http://thedailyshow.cc.com/full-episodes/x6kbau/june-10--2015---colin-quinn"
-//var url = "http://thedailyshow.cc.com/full-episodes/nkl263/june-17--2015---bill-clinton"
 
 // Download queue
 var queue = []
 
-/*
-Formats:
-rtmp-1300
-rtmp-250
-rtmp-450
-rtmp-800
-rtmp-1700
-rtmp-2200
-rtmp-3500
-vhttp-1300
-vhttp-250
-vhttp-450
-vhttp-800
-vhttp-1700
-vhttp-2200
-vhttp-3500
-*/
 
+console.log(JSON.stringify(process.argv))
 
-if(process.argv.length < 2) {
+if(process.argv.length < 3) {
 	console.log("Usage: ")
 	console.log("getdaily.js <url>")
 	process.exit()
-} else {
-	//console.log("process.argv.length: " + process.argv.length)
 }
 
 url = process.argv[2]
 
-console.log("Proceding with url: " + url)
+
 
 function addSlashes(string) {
     return string.replace(/\\/g, '\\\\').
@@ -81,97 +56,109 @@ function makeNatoString(dateString) {
 	return slicedString
 }
 
-var format = "vhttp-200"
-//var format = "vhttp-3500"
+function getGuestName(info) {
+  	var guestName = info[0].playlist_title.split(" - ")[1]
+  	if(guestName == null) {
+  		console.err("unable to parse guest name!")
+  		process.exit(1)
+  	}
+  	guestName = guestName.replace(new RegExp(' ', 'g'), '-')
+  	guestName = guestName.toLowerCase()
 
-//getFormats(format)
-
-downloadAllActs()
-
-function downloadAllActs() {
-	var video = youtubedl(url, null, function(err, info) {
-		if (err) throw err;
-
-		async.eachSeries(
-			info,
-			function(item, callback) {
-				var url
-				var filename = item.upload_date + "_" + item.playlist_index + "." + item.ext
-
-				console.log("---")
-				console.log()
-
-				console.log("Downloading playlist index: " + item.playlist_index)
-				console.log("To filename: " + filename)
-
-				item.formats.every(function(element, index, array) {
-					if(element.format_id == format) {
-						url = element.url
-						console.log("URL for " + format + ": " + element.url)
-						return false
-					}
-					return true
-				})
-
-				if(fs.existsSync(filename)) {
-					console.log("File already exists, skipping")
-					callback()
-				} else {
-					var videoDownload = youtubedl(url, null, null)
-
-					var size = 0;
-					videoDownload.on('info', function(info) {
-						size = info.size
-						console.log('Download started')
-						//console.log('filename: ' + info._filename)
-						console.log('filename: ' + filename)
-						console.log('size: ' + info.size)
-
-						var output = filename
-						console.log("Downloading to " + filename)
-						videoDownload.pipe(fs.createWriteStream(output))
-					})
-
-					var pos = 0;
-					videoDownload.on('data', function(data) {
-						pos += data.length
-						// `size` should not be 0 here.
-						if (size) {
-							var percent = (pos / size * 100).toFixed(2)
-							process.stdout.cursorTo(0)
-							process.stdout.clearLine(1)
-							process.stdout.write(percent + '%')
-						}
-					})
-
-					videoDownload.on('end', function(data) {
-						console.log()
-						console.log("Download completed, doing callback()")
-						callback()
-					})
-				}
-
-			},
-			function(err) {
-				console.log()
-				console.log("Finished extracting urls")
-
-				concatVideo(info)
-			}
-		)
-	})
+  	return guestName
 }
 
-function concatVideo(info) {
+function getUploadDate(info) {
+	var uploadDate = info[0].upload_date
 
-	var sys = require('sys')
-	var exec = require('child_process').exec;
+	return uploadDate
+}
+
+function getTargetFilename(info) {
+	var targetFilename = makeNatoString(getUploadDate(info)) + "_thedailyshow_" + getGuestName(info) + "." + info[0].ext
+
+	return targetFilename
+}
+
+var video = youtubedl(url, null, function(err, info) {
+	if (err) throw err;
+
+	async.eachSeries(
+		info,
+		function(item, callback) {
+			var url
+			var filename = item.upload_date + "_" + item.playlist_index + "." + item.ext
+
+			console.log("---")
+			console.log()
+
+			console.log("Downloading playlist index: " + item.playlist_index)
+			console.log("To filename: " + filename)
+
+			item.formats.every(function(element, index, array) {
+				if(element.format_id == format) {
+					url = element.url
+					console.log("URL for " + format + ": " + element.url)
+					return false
+				}
+				return true
+			})
+
+			if(fs.existsSync(filename)) {
+				console.log("File already exists, skipping")
+				callback()
+			} else {
+				var videoDownload = youtubedl(url, null, null)
+
+				var size = 0;
+				videoDownload.on('info', function(info) {
+					size = info.size
+					console.log('Download started')
+					console.log('filename: ' + filename)
+					console.log('size: ' + info.size)
+
+					var output = filename
+					console.log("Downloading to " + filename)
+					videoDownload.pipe(fs.createWriteStream(output))
+				})
+
+				var pos = 0;
+				videoDownload.on('data', function(data) {
+					pos += data.length
+					// `size` should not be 0 here.
+					if (size) {
+						var percent = (pos / size * 100).toFixed(2)
+						process.stdout.cursorTo(0)
+						process.stdout.clearLine(1)
+						process.stdout.write(percent + '%')
+					}
+				})
+
+				videoDownload.on('end', function(data) {
+					console.log()
+					console.log("Download completed, doing callback()")
+					callback()
+				})
+			}
+
+		},
+		function(err) {
+			console.log()
+			console.log("Finished extracting urls")
+
+			concatVideo(info)
+		}
+	)
+})
+
+function concatVideo(info) {
 
 	if(fs.existsSync(targetFilename)) {
 		console.log(targetFilename + " exists, so not overwriting")
 		return
 	}
 
+	/*
   	var guestName = info[0].playlist_title.split(" - ")[1]
   	if(guestName == null) {
   		console.err("unable to parse guest name!")
@@ -180,6 +167,9 @@ function concatVideo(info) {
 
 
   	guestName = guestName.replace(new RegExp(' ', 'g'), '-')
+
+  	guestName = guestName.toLowerCase()
+  	*/
 
   	//guestName = guestName.replace(" ", "-")
   	//guestName = guestName.replace(" ", "-")
@@ -191,11 +181,13 @@ function concatVideo(info) {
 
 //str = str.replace(re, '');
 
-
-	var uploadDate = info[0].upload_date
-	var targetFilename = makeNatoString(uploadDate) + "_thedailyshow_" + guestName.toLowerCase() + "." + info[0].ext
 	
-	console.log("Checking for the existence of " + targetFilename)
+	//var targetFilename = makeNatoString(getUploadDate(info)) + "_thedailyshow_" + getGuestName(info) + "." + info[0].ext
+
+	var uploadDate = getUploadDate(info)
+	var targetFilename = getTargetFilename(info)
+
+	//console.log("Checking for the existence of " + targetFilename)
 
 	if(fs.existsSync(targetFilename)) {
 		console.log(targetFilename + " already exists, existing")
@@ -251,40 +243,4 @@ function concatVideo(info) {
 	  }
 	})
 
-}
-
-function getFormats(format) {
-	var video = youtubedl(url, null, function(err, info) {
-		if (err) throw err;
-
-		//console.log("---")
-		//console.log(info[0].upload_date)
-		//console.log("---")
-		//console.log("For episode date:" + info[0] )
-
-		async.eachSeries(
-			info,
-			function(item, callback) {
-				var url
-
-				//queue[item.playlist_index] = []
-
-				console.log()
-				console.log("---")
-				console.log("Playlist item: " + item.playlist_index)
-
-				item.formats.every(function(element, index, array) {
-					if(element.format_id == format) {
-						console.log("URL for " + format + ": " + element.url)
-						return false
-					}
-					return true
-				})
-				callback()
-			},
-			function(err) {
-				console.log("Finished extracting urls")
-			}
-		)
-	})
 }
